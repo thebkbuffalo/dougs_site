b2451a94f69ff1bc659f1e28ba8ebaf275606fc8
Evan Berg <evan.m.berg@gmail.com>
2016-01-21 16:41:00 -0800

    sub menu for media good

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   console.log('im ready!');
+
+  $('.sub_nav_content').on('click', function(){
+    $('.main_content_section').removeClass('inactive');
+    $('.image_gallery').addClass('inactive');
+  });
+
+  $('.sub_nav_gallery').on('click', function(){
+    $('.image_gallery').removeClass('inactive');
+    $('.main_content_section').addClass('inactive');
+  });
 });
