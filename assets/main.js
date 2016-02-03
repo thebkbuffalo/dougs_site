23f8e66c59bbfd754f71d2ecbc6b70c013560e40
Evan Berg <evan.m.berg@gmail.com>
2016-02-02 17:10:33 -0800

    video quality is shit

--- assets/main.js
+++ assets/main.js
@@ -9,11 +9,13 @@ $(document).ready(function(){
     $('.main_content_section').addClass('inactive');
   });
 
-  $('.fancybox').fancybox();
 
   $('.continue').on('click', function(){
     $('video').prop('muted', true)
     $('#hero').hide(500);
     $('#index_page').css('display', 'block');
   });
+
+
+  $('.fancybox').fancybox();
 });
